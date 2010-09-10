aeae072fe5d6ae643647e53e769272a6611fccbe
Terje Loken <terjelo@gmail.com>
2010-09-11 00:52:40 +0200

    Pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe PagesController do
+  integrate_views
 
   #Delete these examples and add some real ones
   it "should use PagesController" do
@@ -13,6 +14,11 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'home'
+      response.should have_tag("title",
+                               "Ruby on Rails Tutorial sample app | Home")
+    end
   end
 
   describe "GET 'contact'" do
@@ -20,5 +26,23 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'contact'
+      response.should have_tag("title",
+                               "Ruby on Rails Tutorial sample app | Contact")
+    end
   end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'about'
+      response.should have_tag("title",
+                               "Ruby on Rails Tutorial sample app | About")
+    end
+  end
+
 end
